8f7b7ba43ca3d500cd53bca6a5cdbc2c05ebc3c8
Aravindambalavanan <j_aravind@outlook.com>
2023-09-03 22:15:36 +0530

    Updated link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import io from "socket.io-client";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-app-server-qmpd.onrender.com");
 
 function App() {
   const [userName, setUserName] = useState("");
